8d9884ae9c62abf88f4c30d0eff6c946c85a529c
mikonaft <mikonaft@gmail.com>
2023-01-10 17:59:18 +0600

    Tengrinews Parsing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-
 import json
 from bs4 import BeautifulSoup
 import requests
@@ -16,25 +15,31 @@ with open("core/html/index.html", "r") as file:
 soup = BeautifulSoup(src, "html.parser")
 
 news = soup.find_all("div", class_="tn-news-author-list-item")
-teg_img = soup.find_all('img')
+teg_image = soup.find_all("div", class_="tn-image-container")
 
 news_info = []
 for item in news:
-    title = item.find("div", class_="tn-news-author-list-item-text").find("span", class_="tn-news-author-list-title")
-    description = item.find("div", class_="tn-news-author-list-item-text").find("p", class_="tn-announce")
-    date_time = item.find("div", class_="tn-news-author-list-item-text").find("li")
-    news_url = DOMEN + item.find("a").get("href")
-    
-    for image in teg_img:
-        src = image.get("src")
-        if src:
-            image = DOMEN + src
-    
-    information = {
+    try:
+        title = item.find("div", class_="tn-news-author-list-item-text").find("span", class_="tn-news-author-list-title")
+        description = item.find("div", class_="tn-news-author-list-item-text").find("p", class_="tn-announce")
+        date_time = item.find("div", class_="tn-news-author-list-item-text").find("li")
+        news_url = DOMEN + item.find("a").get("href")
+        image = item.find("div", class_="tn-image-container").find("img").get("src")
+    except:
+        image = item.find("div", class_="tn-video-container").find("source").get("src")
+        information = {
         "title": title.text,
         "description": description.text,
         "date_time": date_time.text.strip(),
-        "image": image,
+        "image": DOMEN + image,
+        "url": news_url
+    }
+    else:
+        information = {
+        "title": title.text,
+        "description": description.text,
+        "date_time": date_time.text.strip(),
+        "image": DOMEN + image,
         "url": news_url
     }
     
