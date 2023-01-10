83bc5a99162368c76526edd502de320aefe7a166
Dezmoral <warsatar22822@mail.ru>
2023-01-10 18:05:03 +0600

    Tengrinews Parsing 1

---
+++ core/config.py
@@ -0,0 +1,6 @@
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
+}
+DOMEN = "https://tengrinews.kz"
+URL = " https://tengrinews.kz/tag/алматы/"
