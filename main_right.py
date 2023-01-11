d8a21d3112756580a7952b78425f3315b0eacb75
Abay <120572718+HiggsBoson8@users.noreply.github.com>
2023-01-11 15:37:28 +0600

    Update main_right.py

--- main_right.py
+++ main_right.py
@@ -1,7 +1,10 @@
-# описание
-#название
-#фотография
-#когда была опубликована
+# описание 
+# название статьи
+# фотография(+видео(except:))
+# дата публикации
+# ссылка на статью/новость.
+# "tn-news-author-list-item-image" and "tn-image-container" - difference? need to find out(!)
+
 
 import json
 import csv
@@ -10,11 +13,11 @@ import requests
 from core.config import URL, DOMEN, HEADERS
 
 
-# response = requests.get(url=URL, headers=HEADERS)
-# print(response.status_code)
-# src = response.text
-# with open("core/html/index.html", "w") as file:
-#     file.write(src)
+response = requests.get(url=URL, headers=HEADERS)
+# print(response.status_code) 
+src = response.text 
+with open("core/html/index.html", "w") as file: 
+    file.write(src) 
 
 with open("core/html/index.html", "r") as file:
     src = file.read()
@@ -22,39 +25,37 @@ with open("core/html/index.html", "r") as file:
 soup = BeautifulSoup(src, "html.parser")
 
 news = soup.find_all("div", class_="tn-news-author-list-item")
-teg_img = soup.find_all('img')
+teg_img = soup.find_all("div", class_ = "tn-image-container")
 
 news_info = []
 for item in news:
-    title = item.find("div", class_="tn-news-author-list-item-text").find("span", class_="tn-news-author-list-title")
-    description = item.find("div", class_="tn-news-author-list-item-text").find("p", class_="tn-announce")
-    date_time = item.find("div", class_="tn-news-author-list-item-text").find("li")
-    news_url = DOMEN + item.find("a").get("href")
-    
-#     print(f"""
-# {title.text} 
-# {description.text}
-# {date_time.text} 
-# {news_url}\n""")
-    for image in teg_img:
-        src = image.get("src")
-        if src:
-            image = DOMEN + src
-    
-    information = {
+    try:
+        title = item.find("div", class_ = "tn-news-author-list-item-text").find("span", class_ = "tn-news-author-list-title")  # статья и название статьи
+        description = item.find("div", class_ = "tn-news-author-list-item-text").find("p", class_ = "tn-announce") # статья и содержание статьи
+        date_time = item.find("div", class_ = "tn-news-author-list-item-text").find("li") # статья и дата публикации статьи
+        news_url = DOMEN + item.find("a").get("href") # сссылка на статью
+        image = item.find("div", class_ = "tn-image-container").find("img").get("src") # ссылка на картинку
+    except: 
+        image = item.find("div", class_ = "tn-video-container").find("source").get("src") # видео(!)
+        information = {
         "title": title.text,
         "description": description.text,
         "date_time": date_time.text.strip(),
-        "image": image,
+        "image": DOMEN + image,
         "url": news_url
-    }
-    
-    news_info.append(information)
+        }
+    else:
+        information = {
+        "title": title.text,
+        "description": description.text,
+        "date.time": date_time.text.strip(),
+        "image": DOMEN + image,
+        "url": news_url
+        }
 
-with open(f"core/json/tengrinews.json", "w", encoding="utf-8") as file:
-    json.dump(news_info, file, indent=4, ensure_ascii=False)
+        news_info.append(information)
 
 
-
-
+with open(f"core/json/tengrinews.json", "w", encoding = "utf-8") as file:
+    json.dump(news_info, file, indent = 4, ensure_ascii = False)
 
